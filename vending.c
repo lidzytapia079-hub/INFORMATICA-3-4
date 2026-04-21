9ed5a991e17d50330d89f9fc7e3f58deb9cd574b
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-04-21 15:49:03 +0000

    vending process

---
+++ vending.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+    //variables for snickers
+    float price1 = 28.98;
+    int quantity1 = 16;
+    char select1 = 's';
+    //variables for reese's
+    float price2 = 29.86;
+    int quantity2 = 8;
+    char select2 = 'r';
+    //variables for Dr.Pepper
+    float price3 = 25.59;
+    int quantity3 = 11
+    //print stock of vending machine
+    printf
+
+printf("item name:snickers/n");
+}
