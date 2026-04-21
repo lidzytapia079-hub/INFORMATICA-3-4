b02358183352a0b7505abace014a074f39ea102e
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-04-21 15:23:58 +0000

    naming variables  complete

---
+++ naming-variables.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+    int number;//i put out the excalmation sign cinform by the rules.
+int test_value;//i change the space between test and value with an underscore conform to the rules..
+int example1;//I change the numper one before example to after od the word example because we can't start a word witha number.
+float int_;// I put a underscore like int_ for that word left be a key word.
+printf("Variables created successfully.\n");
+}
