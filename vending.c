5c5124489ad99811388afd24dcd4f8cc1fdf18ae
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-04-23 17:49:42 +0000

    vending final complete

--- vending.c
+++ vending.c
@@ -12,26 +12,26 @@ int main(void){
     //variables for Dr.Pepper
     float price3 = 25.59;
     int quantity3 = 11;
-    char quantity3 = 'p';
+    char select3 = 'p';
     //print stock of vending machine
     printf("vending machine stock\n");
     printf("--------------------------\n");
     printf("item name:snickers\n");
-    printf("price = %f \n" , price1);
+    printf("price = %.2f \n" , price1);
     printf("quantity = %d \n" , quantity1);
     printf("char = %c \n" , select1);
 
     printf("-------------------------\n");
     printf("item name :reese's\n");
-    printf("price = %f \n", price2);
+    printf("price = %.2f \n", price2);
     printf(" quantity = %d \n", quantity2);
     printf("char = %c \n", select2);
 
     printf("-------------------------\n");
     printf("item name : Dr. Pepper\n");
-    printf("price = %f \n", price3);
+    printf("price = %.2f \n", price3);
     printf("quantity = %d \n", quantity3);
-    printf("char = %c \n", char3);
+    printf("char = %c \n", select3);
 
 
 
