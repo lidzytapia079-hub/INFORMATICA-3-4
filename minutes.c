07d1f4ab99bdf0feb51a58530dcd36dc59cdd7b9
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-04-28 18:09:07 +0000

    minutes 5

--- minutes.c
+++ minutes.c
@@ -5,7 +5,19 @@ int main(void){
     float  hours;
     printf("Enter the number of minutes:\n");
     scanf("%f", &minutes);
-  hours =(minutes)(/60)-60;
+     hours =(minutes)/60;
+     minutes = minutes (%60);
     printf(" hours =%f\n", hours);
+    int minutes ;
+    int start time;
+    printf("Enter the run time of the movie in minutes:\n");
+    scanf("%d", &minutes);
+    printf("Enter the time you start the movie:\n");
+    printf("\n")
+    int runtime= minutes/60;
+    int runtime2=minutes%60;
+    int endtime= start time +runtime;
+    int endtime2= start time +runtime2;
+    printf("the run time)
     }
 
