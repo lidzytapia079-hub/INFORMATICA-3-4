fb689eb624642402127a4f29e22c1ac045e6ef88
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-04-28 15:48:25 +0000

    minutes process

---
+++ minutes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    float  minutes;
+    float  hours;
+    printf("Enter the number of minutes:\n");
+    scanf("%f", &minutes);
+  hours =(minutes)(/60)-60;
+    printf(" hours =%f\n", hours);
+    }
+
