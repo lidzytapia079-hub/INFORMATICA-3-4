40bd0bc33ac0d63404ebb696a6ca9dffad5d516f
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-04-29 18:09:05 +0000

    munutes casi terminado

--- minutes.c
+++ minutes.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
 
 int main(void){
-    float  minutes;
-    float  hours;
-    printf("Enter the number of minutes:\n");
-    scanf("%f", &minutes);
-     hours =(minutes)/60;
-     minutes = minutes (%60);
-    printf(" hours =%f\n", hours);
+
     int minutes ;
-    int start time;
+    int starttime;
     printf("Enter the run time of the movie in minutes:\n");
     scanf("%d", &minutes);
     printf("Enter the time you start the movie:\n");
-    printf("\n")
+    scanf("%d", &starttime);
+    printf("\n");
     int runtime= minutes/60;
     int runtime2=minutes%60;
-    int endtime= start time +runtime;
-    int endtime2= start time +runtime2;
+    int endtime= starttime +runtime;
+    int endtime2= starttime +runtime2;
     printf("the run time  of the movie is %d hours and %d minutes\n" , runtime , runtime2);
-    printf("you will finish the movie at %d:%d\n, endtime,endtime2);
+    printf("you will finish the movie at %d:%d\n", endtime,endtime2);
     }
 
