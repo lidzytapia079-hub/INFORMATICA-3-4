51273016879308f897691b5f2a2c9bbd8cc61f53
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-04-29 14:59:19 +0000

    test

---
+++ test/receipt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void);
+//Variables for items
+char item1=orange;
+int quantity1 = 6;
+float price1= 5.67;
+int = scanf quantity1*price1;
+//printf
+printf("************************\n");
+printf("alsuper grocery store\n");
+printf("************************\n");
+print(" %c\n" ,item1);
+printf("%d",quantity1);
+printf("%.2f" , price1);
+printf("----------------------------"\n);
+()
+
