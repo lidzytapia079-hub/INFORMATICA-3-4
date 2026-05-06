40664ffeb5207e0ad74b88a44233f5ed799eae9b
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-05-06 14:55:57 +0000

    ifs complete

---
+++ ifs.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(void){
+    int a = 5; //Equal sign means assignment
+    int b = 5;
+    int c =10;
+
+    printf("%d == %d is %d \n",a,b, a==b);  //Equal sign means assignment
+    printf("%d != %d is %d \n",a,c, a!=c);  //!=means "not equal"
+
+    //Create a program to check if a number is negative
+    int number;
+    printf("Enter a number :");
+    scanf("%d", &number);
+
+    if(number <0){
+        printf("%d is a number.\n", number);
+        }else{
+            printf("%d is a positive  number.\n", number);
+
+        }
+
+
+
+
+
+
+
+}
