8cb5552817c22db881574ecf43d20d79c5310d80
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-05-07 20:19:33 +0000

    temperature2

---
+++ temp2.c
@@ -0,0 +1,11 @@
+#int<stdio>
+
+main (void){
+float Celcius;
+printf("enter  your temperature in Celsius:");
+scanf("%f", &celcius);
+float Fahrenheit=(celcius*1.8)+32;
+if(Fahrenheit<0){
+    print("Freezing temperature❄️\n");
+}
+
