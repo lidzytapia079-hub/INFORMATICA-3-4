bfe939e9bcc69d53001e349ccc2058ef37a8ebe0
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-05-07 14:56:10 +0000

    minutes77

--- week 2/minutes.c
+++ week 2/minutes.c
@@ -2,10 +2,10 @@
 
 int main(void){
 
-    int minutes ;
+    int time;
 
     printf("Enter the run time of the movie in minutes:\n");
-    scanf("%d", &minutes);
+    scanf("%d", &time);
 
     int start_h;
     int start_m;
@@ -13,13 +13,18 @@ int main(void){
     scanf("%d:%d", &start_h, &start_m);
 
 
-    int hours= minutes/60;
+    int time= minutes/60;
     int minutes=minutes%60;
 
-    int endtime_h= start_h +hours;
-    int endtime_m= start_m+minutes;
+    int endtime_h= start_h +hours;//end_h=8
+    int endtime_m= start_m+minutes; //end_m=61
+
+    if(end_m >=60){
+        end_h=end_h+(end_m/60);//update variable value
+        end_m=end_m % 60;//variable value plus the reminder of 60
+    }
 
     printf("the movie will last %d hours and %d minutes\n" ,  hours, minutes);
-    printf("end time: %d:%d\n", endtime_h,endtime_m);
+    printf("end time: %d:%02d\n", endtime_h,endtime_m);
     }
 
