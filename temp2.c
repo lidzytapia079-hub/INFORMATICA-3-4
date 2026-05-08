35fe9b14150edecfc152e10a95f3ac6e22597863
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-05-08 20:53:02 +0000

    temp2 finish

--- temp2.c
+++ temp2.c
@@ -1,11 +1,29 @@
-#int<stdio>
+#include<stdio.h>
 
-main (void){
-float Celcius;
-printf("enter  your temperature in Celsius:");
-scanf("%f", &celcius);
-float Fahrenheit=(celcius*1.8)+32;
-if(Fahrenheit<0){
-    print("Freezing temperature❄️\n");
+int main (void){
+float celsius;
+printf("enter  your temperature in celsius:");
+scanf("%f", &celsius);
+float Fahrenheit=(celsius*1.8)+32;
+if(celsius<0){
+    printf("Freezing temperature❄️\n");
+
 }
+else if (celsius<=10){
+    printf("🥶 Very cold weather\n");
+}
+else if (celsius<=20){
+    printf("🧥 Chilly weather\n");
+}
+else if (celsius<=30){
+    printf("🖼️ Normal weather\n");
+}
+else if (celsius<=40){
+    printf("☀️ Hot weather\n");
+}
+else{
+    printf(" Very hot weather\n");
+}
+printf("%.2fC \n", Fahrenheit, celsius);
 
+}
