bd89c4bbe82e0f4ec79a6277dbaaa3442180f63e
lidzytapia079-hub <lidzytapia079@gmail.com>
2026-05-12 14:45:17 +0000

    calc2

--- calc.c
+++ calc.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+//1.prototype
+void multiply( void);
+void divide(void);
+void add(void);
+void substract(void);
 
+int main(void){
+    float num1;
+    float num2;
+    float num3;
+    intuser_response;
+    printf("calculator\n");
+    printf("enter your number");
+    scanf(%d)
+}
